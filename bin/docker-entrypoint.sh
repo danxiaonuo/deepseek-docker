5dffd1e29906866b7b68225f71cfaf28a2d53af9
danxiaonuo <danxiaonuo@danxiaonuo.me>
2025-02-06 15:04:01 +0800

    Update docker-entrypoint.sh

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,5 +2,5 @@
 
 ollama serve &  # 在后台启动 Ollama
 sleep 3         # 等待其初始化
-ollama run deepseek-r1  # 运行 大模型
-tail -f /dev/null  # 保持容器运行
+# 运行supervisord
+exec /usr/bin/python3 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
